7dcac2f7640f75490d32c27fea533beef371fe43
Michael Cardona <michaelcx0624@gmail.com>
2020-12-12 11:51:38 -0800

    javascript primitive and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,25 @@
+var fullName;
+fullName = 'Michael Cardona';
+
+var isCool;
+isCool = true;
+
+var totalPets;
+totalPets = 2;
+
+console.log('Full Name:', fullName);
+console.log('Is Cool:', isCool);
+console.log('Total Pets:', totalPets);
+
+console.log('typeof fullName:', typeof 'Michael Cardona');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets:', typeof 2);
+
+var unknown;
+unknown = null;
+console.log('Unknown:', unknown);
+console.log('typeof Unknown:', typeof null);
+
+var never;
+console.log('Never', never);
+console.log('typeof Never:', typeof never);
