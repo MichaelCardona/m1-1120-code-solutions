ae8f8ac68c5c5be7dffe0ba17a45bef06dce16d3
Michael Cardona <michaelcx0624@gmail.com>
2020-12-17 20:26:20 -0800

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,32 @@
+var orderHistory = [
+  {
+    orderNum: '114-3941689-8772232',
+    itemName: 'JavaScript for impatient programmer',
+    totalPrice: 34.00,
+    author: 'Rauschmayer, Dr. Axel',
+    dateOrder: 'August 4, 2020'
+  },
+  {
+    orderNum: '113-9984268-120257',
+    itemName: 'The Timeless Way of Building',
+    totalPrice: 44.53,
+    author: 'Alexander Christopher',
+    dateOrder: 'July 19, 2020'
+  },
+  {
+    orderNum: '114-2875557-9059409',
+    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for the Wii U,PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    totalPrice: 12.22,
+    dateOrder: 'July 4, 2020'
+  },
+  {
+    orderNum: '113-2883177-2648248',
+    itemName1: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+    price1: 94.95,
+    itemName: 'The Art of SQL',
+    price: 33.99,
+    totalPrice: 138.93,
+    dateOrder: 'July 4, 2020'
+  }
+
+];
